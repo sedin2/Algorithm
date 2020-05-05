619261d6f6aa02c2c8d91b6641ea5a6a83226dd7
sedin <leesejinn@naver.com>
2020-05-05 15:59:04 +0900

    [Programmers]  문자열 다루기 기본
    - Level1

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -2,29 +2,16 @@ package Programmers;
 
 public class Solution {
 
-    public int solution(int n) {
-        int answer = 0;
-
-        int[] number = new int[n + 1];
-
-        for (int i = 2; i <= n; i++) {
-            number[i] = i;
+    public boolean solution(String s) {
+        boolean answer = true;
+        if (!(s.length() == 4 || s.length() == 6)) {
+            return false;
         }
-
-        for (int i = 2; i <= n; i++) {
-            if (number[i] == 0) continue;
-
-            for (int j = 2 * i; j <= n; j += i) {
-                number[j] = 0;
-            }
+        try{
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
         }
-
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] != 0) {
-                answer++;
-            }
-        }
-
         return answer;
     }
 }
