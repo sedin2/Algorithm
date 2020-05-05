c2f150e0b6e2e683a0e8bb33fb3ec797ae668976
sedin <leesejinn@naver.com>
2020-05-05 16:49:10 +0900

    [Programmers]  탑
    - Level2

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,16 +1,28 @@
 package Programmers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
-    public boolean solution(String s) {
-        boolean answer = true;
-        if (!(s.length() == 4 || s.length() == 6)) {
-            return false;
+    public int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+        Map<Integer, Integer> tmp = new HashMap<>();
+        for (int i = heights.length - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (heights[i] < heights[j]) {
+                    tmp.put(i, j + 1);
+                    break;
+                }
+            }
         }
-        try{
-            Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            return false;
+        for (int i = 0; i < heights.length; i++) {
+            if (tmp.get(i)==null) {
+                answer[i] = 0;
+            }
+            else {
+                answer[i] = tmp.get(i);
+            }
         }
         return answer;
     }
