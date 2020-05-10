68bdf6454a5ccd49150900929dbcd81ddcd507d1
sedin <leesejinn@naver.com>
2020-05-10 23:11:18 +0900

    Refactor Main.class
    - Main class 초기화

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,9 @@
 package com.company;
 
-import Leetcode.Solution;
+import Programmers.Solution;
 
 public class Main {
     public static void main(String[] args) {
-        Solution answer = new Solution();
-        //String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        //String[] completion = {"stanko", "ana", "mislav"};
-
-        //System.out.println(answer.solution(participant, completion));
-        System.out.println(answer.reverse(-123));
-        System.out.println(answer.reverse(-120));
-        System.out.println(answer.reverse(123));
-        System.out.println(answer.reverse(120));
-        System.out.println(answer.reverse(0));
+        
     }
 }
