d0a7df46285619e2c2fb6abcb090db76d657977a
sedin <leesejinn@naver.com>
2020-05-19 23:13:01 +0900

    [Programmers]  두 정수 사이의 합
    - Level1

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,24 +1,20 @@
 package Programmers;
 
-public class Solution {
-
-    public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-
-        for (int i = 0; i < prices.length - 1; i++) {
-            int count = 0;
-            for (int j = i + 1; j < prices.length; j++) {
-                if (prices[i] <= prices[j]) {
-                    count++;
-                } else {
-                    count++;
-                    break;
-                }
-            }
-            answer[i] = count;
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if (a == b) {
+            return a;
         }
-        for (int i = 0; i < 2; i++) {
-            answer[answer.length - i - 1] = i;
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                answer += i;
+            }
+        }
+        if (a > b) {
+            for (int i = b; i <= a; i++) {
+                answer += i;
+            }
         }
         return answer;
     }
