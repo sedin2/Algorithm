a0b60fdf4f2586a22dff1cc4a4db36eada0c7e13
sedin <leesejinn@naver.com>
2020-05-23 11:51:50 +0900

    [Programmers]  아름다운 년도
    - Level1

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,21 +1,38 @@
 package Programmers;
 
-class Solution {
-    public long solution(int a, int b) {
-        long answer = 0;
-        if (a == b) {
-            return a;
-        }
-        if (a < b) {
-            for (int i = a; i <= b; i++) {
-                answer += i;
+public class Solution {
+    public int solution(int p) {
+        int answer = 0;
+        for(int i=p+1; i<10000; i++) {
+            if(isBeauty(i)) {
+                answer = i;
+                break;
             }
         }
-        if (a > b) {
-            for (int i = b; i <= a; i++) {
-                answer += i;
+        return answer;
+    }
+
+    private boolean isBeauty(int p) {
+        boolean answer = true;
+        String b = String.valueOf(p);
+        String[] c = new String[b.length()];
+
+        for (int i = 0; i < b.length(); i++) {
+            c[i] = String.valueOf(b.charAt(i));
+        }
+
+        for (int i = 0; i < c.length - 1; i++) {
+            answer = true;
+            for (int j = i + 1; j < c.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (c[i].equals(c[j])) {
+                    return false;
+                }
             }
         }
+
         return answer;
     }
 }
