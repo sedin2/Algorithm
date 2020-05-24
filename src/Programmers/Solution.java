305abcd49f0334afda9aabfb3efed8c2f0916609
sedin <leesejinn@naver.com>
2020-05-24 23:00:45 +0900

    [Programmers]  평균 구하기
    - Level1

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,38 +1,11 @@
 package Programmers;
 
 public class Solution {
-    public int solution(int p) {
-        int answer = 0;
-        for(int i=p+1; i<10000; i++) {
-            if(isBeauty(i)) {
-                answer = i;
-                break;
-            }
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i: arr) {
+            answer += i;
         }
-        return answer;
-    }
-
-    private boolean isBeauty(int p) {
-        boolean answer = true;
-        String b = String.valueOf(p);
-        String[] c = new String[b.length()];
-
-        for (int i = 0; i < b.length(); i++) {
-            c[i] = String.valueOf(b.charAt(i));
-        }
-
-        for (int i = 0; i < c.length - 1; i++) {
-            answer = true;
-            for (int j = i + 1; j < c.length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (c[i].equals(c[j])) {
-                    return false;
-                }
-            }
-        }
-
-        return answer;
+        return answer/arr.length;
     }
 }
