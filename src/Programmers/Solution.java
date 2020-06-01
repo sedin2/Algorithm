71bc4da829973d1645a25c8b017bf10faaad5a6e
sedin <leesejinn@naver.com>
2020-06-01 22:54:38 +0900

    [Programmers] 가장 큰 수
    - Level2
    - SQL

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,21 +1,29 @@
 package Programmers;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solution {
-    public String solution(String[] participant, String[] completion) {
+    public String solution(int[] numbers) {
+        String answer = "";
 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-
-        int i;
-
-        for (i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
-            }
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = (String.valueOf(numbers[i]));
         }
 
-        return participant[i];
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2 + s1).compareTo(s1 + s2);
+            }
+        });
+
+        if (arr[0].equals("0")) return "0";
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer;
     }
 }
