1f9ac8aaa4c358a373d5525812e1917098b0b01c
sedin <leesejinn@naver.com>
2020-06-03 21:37:46 +0900

    [Programmers] 124 나라의 숫자
    - Level2
    - SQL

--- src/Programmers/Solution.java
+++ src/Programmers/Solution.java
@@ -1,28 +1,18 @@
 package Programmers;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class Solution {
-    public String solution(int[] numbers) {
+    public String solution(int n) {
         String answer = "";
+        int tmp;
+        while (n > 0) {
+            tmp = n%3;
+            n = n/3;
 
-        String[] arr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            arr[i] = (String.valueOf(numbers[i]));
-        }
-
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return (s2 + s1).compareTo(s1 + s2);
+            if(tmp == 0) {
+                n -= 1;
+                tmp = 4;
             }
-        });
-
-        if (arr[0].equals("0")) return "0";
-
-        for (int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+            answer = tmp + answer;
         }
         return answer;
     }
