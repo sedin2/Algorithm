7d1b271a4dbc38e1d80108df383c3b23c912aadb
sedin <leesejinn@naver.com>
2020-06-21 20:53:14 +0900

    [Programmers] 중성화 여부 파악하기
    - Level2

--- src/Solution.sql
+++ src/Solution.sql
@@ -1,1 +1,2 @@
-SELECT animal_id from animal_ins where name != 'NULL'
+SELECT ANIMAL_ID, NAME, (SELECT IF(SEX_UPON_INTAKE = 'Neutered Male', 'O', IF(SEX_UPON_INTAKE = 'Spayed Female', 'O', 'X'))) as '중성화'
+from animal_ins order by animal_id asc;
